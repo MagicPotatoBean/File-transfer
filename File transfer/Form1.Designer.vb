87642b356555055e168065049addbdb264fca487
MagicPotatoBean <leo@soutter.com>
2023-04-10 20:54:08 +0100

    Initial commit

---
+++ File transfer/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(206, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 41)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Receive"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(118, 42)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(230, 77)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "File transfer"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ File transfer/Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub SendFile(FileName As String)
+
+
+        Try
+
+
+
+
+            Dim Input As Boolean = False
+            Dim IP As String = InputBox("Enter the IP address or hostname of your target", "IP Address", "127.0.0.1")
+            Dim Port As Integer = InputBox("Enter the Port of your target", "Port", "13333")
+            Dim Client As New TcpClient
+            Client.Connect(IP, Port)
+            MsgBox("Device connected")
+            Input = True
+            Dim NWStream As NetworkStream = Client.GetStream
+            Dim bytesToSend(Client.SendBufferSize) As Byte
+            Dim FI As New FileInfo(FileName)
+            ProgressBar1.Maximum = FI.Length
+            Dim FileSTR As New FileStream(FileName, FileMode.Open, FileAccess.Read)
+            Dim FileReader As New BinaryReader(FileSTR)
+            Dim numBytesRead As Integer
+            Dim Ipos As Integer
+
+
+            Do Until Ipos >= FI.Length
+                numBytesRead = FileSTR.Read(bytesToSend, 0, bytesToSend.Length)
+                NWStream.Write(bytesToSend, 0, numBytesRead)
+                Ipos = Ipos + numBytesRead
+                ProgressBar1.Value += numBytesRead
+                ProgressBar1.Update()
+                NWStream.Flush()
+            Loop
+            MsgBox("File Sent Successfully")
+            NWStream.Flush()
+            NWStream.Close()
+            FileSTR.Close()
+            FileReader.Close()
+            Enabled = True
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub ReceiveFile(FileName As String)
+        Try
+
+            If File.Exists(FileName) Then
+                File.Delete(FileName)
+            End If
+
+            Dim IP As String = GetIPv4Address()
+
+            Dim TCP As New TcpListener(IPAddress.Any, InputBox("Enter port you would like to use.", "Port", "13333"))
+            TCP.Start()
+
+
+            Dim Client As TcpClient = TCP.AcceptTcpClient()
+            Dim NWStream As NetworkStream = Client.GetStream
+            Dim bytesToRead(Client.ReceiveBufferSize) As Byte
+            Dim numBytesRead As Integer
+            Dim BUFFER_SIZE As Integer = Client.ReceiveBufferSize
+            Dim FileSTR As New FileStream(FileName, FileMode.Append, FileAccess.Write)
+            Dim BinWrite As New BinaryWriter(FileSTR)
+            Dim Bytes As New Integer
+            Do
+                numBytesRead = 0
+                numBytesRead = NWStream.Read(bytesToRead, 0, BUFFER_SIZE)
+                BinWrite.Write(bytesToRead, 0, numBytesRead)
+                Bytes = Bytes + numBytesRead
+            Loop Until numBytesRead = 0
+
+            FileSTR.Close()
+            NWStream.Close()
+            Client.Close()
+            MsgBox("File saved to [" & FileName & "]")
+            Enabled = True
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        With SaveFileDialog1
+            .Title = ""
+            .ShowDialog()
+        End With
+        Dim ThreadReceive As New Thread(AddressOf ReceiveFile)
+        ThreadReceive.Start(SaveFileDialog1.FileName)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        With OpenFileDialog1
+            .CheckFileExists = True
+            .Title = ""
+            .ShowDialog()
+        End With
+        Dim ThreadReceive As New Thread(AddressOf SendFile)
+        ThreadReceive.Start(OpenFileDialog1.FileName)
+    End Sub
+
+    Private Function GetIPv4Address() As String
+        GetIPv4Address = String.Empty
+        Dim strHostName As String = System.Net.Dns.GetHostName()
+        Dim iphe As System.Net.IPHostEntry = System.Net.Dns.GetHostEntry(strHostName)
+
+        For Each ipheal As System.Net.IPAddress In iphe.AddressList
+            If ipheal.AddressFamily = System.Net.Sockets.AddressFamily.InterNetwork Then
+                GetIPv4Address = ipheal.ToString()
+            End If
+        Next
+
+    End Function
+End Class
+
+
